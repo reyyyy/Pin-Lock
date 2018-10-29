e38dca2814b007824bd060595c2469e4205f2fcf
unknown <setiadireynaldi@gmail.com>
2018-10-29 11:21:13 +0700

    coba

--- mylibrary/src/main/java/com/setiadi/meong/reynaldi/mylibrary/CustomToast.java
+++ mylibrary/src/main/java/com/setiadi/meong/reynaldi/mylibrary/CustomToast.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class CustomToast {
 
-    public static void customToast(Context c,String message){
+    public static void c(Context c,String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
